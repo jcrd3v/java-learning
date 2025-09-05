0bd86dc08a290c14302369226a51639bb58a88b5
jcrd3v <jcr.dev@icloud.com>
2025-09-05 08:38:07 -0500

    to be continued from exercise 4 from Claude, check it please and finish the exercises

--- Ejercicios/C2/CharMath.java
+++ Ejercicios/C2/CharMath.java
@@ -1,5 +1,15 @@
 public class CharMath {
     public static void main(String[] args) {
+        char ch = 'A' + 25;
+        int limite = 'A' + 25;
 
+        System.out.println(limite);
+        System.out.println(ch + " " + (int)ch);
+        System.out.println((ch + 1) + " " + (int)ch);
+
+        for (char cc = 'A'; cc < limite; cc++) {
+            cc = cc + 3;
+            System.out.println(cc);
+        }
     }
 }
